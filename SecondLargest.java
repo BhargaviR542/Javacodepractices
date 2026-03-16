22dd8c18c059e62c3d3952e9c9ef3f41a4d269c4
Bhargavi Ravipati <bhargaviravipati5@gmail.com>
2026-03-15 19:37:56 -0500

    Add files via upload

---
+++ SecondLargest.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        
+        if(arr == null || arr.length < 2) {
+            return -1;
+        }
+        int max = Integer.MIN_VALUE; int secondMax=Integer.MIN_VALUE;
+        for(int i=0; i< arr.length; i++) {
+            if(arr[i]>max) {
+                secondMax=max;
+                max=arr[i];
+            } else if(arr[i] > secondMax && arr[i] < max) {
+                secondMax = arr[i];
+            }
+        }
+        return (secondMax == Integer.MIN_VALUE) ? -1 : secondMax;
+    }
+}
